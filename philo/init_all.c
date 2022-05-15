7296b078e2191d1f8ffdcdd9864b05b2305fc6ce
ElMariuso <thiry.marius@gmail.com>
2022-05-15 20:26:48 +0200

    Update

--- philo/init_all.c
+++ philo/init_all.c
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 20:57:47 by mthiry            #+#    #+#             */
-/*   Updated: 2022/05/15 16:58:15 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/05/15 20:26:07 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int init_all(t_rules *rules, char   **argv)
 {
     rules->nb_philo = ft_atoi(argv[1]);
-    rules->time_die = ft_atoi(argv[2]);
-    rules->time_eat = ft_atoi(argv[3]);
-    rules->time_sleep = ft_atoi(argv[4]);
-    if (rules->nb_philo <= 0 || rules->time_die <= 0 
-    || rules->time_eat <= 0 || rules->time_sleep <= 0)
+    rules->time_to_die = ft_atoi(argv[2]);
+    rules->time_to_eat = ft_atoi(argv[3]);
+    rules->time_to_sleep = ft_atoi(argv[4]);
+    if (rules->nb_philo <= 0 || rules->time_to_die <= 0 
+    || rules->time_to_eat <= 0 || rules->time_to_sleep <= 0)
         return (1);
     if (argv[5] != NULL)
     {
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 19:06:01 by mthiry            #+#    #+#             */
-/*   Updated: 2022/05/15 16:48:53 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/05/15 20:25:51 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,25 @@
 
 typedef struct s_rules
 {
-    int nb_philo;
-    int time_die;
-    int time_eat;
-    int time_sleep;
-    int nb_time_philo_eat;
+    int     nb_philo;
+    int     time_to_die;
+    int     time_to_eat;
+    int     time_to_sleep;
+    int     nb_time_philo_eat;
+    bool    all_eat;
     //forks
     //philos
 } t_rules;
+
+typedef struct s_philo
+{
+    int         id;
+    int         nb_eat;
+    int         right_fork;
+    int         left_fork;
+    t_rules     rules;
+    pthread_t   thread;
+} t_philo;
 
 int     print_error(char    *message);
 int     init_all(t_rules *rules, char   **argv);
