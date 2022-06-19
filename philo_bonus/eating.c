41ba6ca7f71aadf3b8899b4edd9ce9458d329dba
Marius Thiry <mthiry@fu-r6-p4.s19.be>
2022-06-19 13:26:17 +0200

    Update

--- philo_bonus/eating.c
+++ philo_bonus/eating.c
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 16:58:23 by mthiry            #+#    #+#             */
-/*   Updated: 2022/06/15 15:20:46 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/06/19 13:23:10 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@ void	eating(t_philo *philo, t_rules *rules)
 {
 	sem_wait(rules->forks);
 	print_action(rules, philo, philo->id, "has taken a fork");
+	if (rules->nb_philo == 1)
+	{
+		while (death_checker(rules, philo) != 1)
+		{
+		}
+		sem_post(rules->forks);
+		return ;
+	}
 	sem_wait(rules->forks);
 	print_action(rules, philo, philo->id, "has taken a fork");
 	sem_wait(rules->data_race_checker);
